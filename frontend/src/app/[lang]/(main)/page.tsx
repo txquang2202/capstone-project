1bf1920dc3e23d6907292116621e7dae51d79ee7
jacknguyen <nguyenphucbao098@gmail.com>
2024-02-27 13:54:16 +0700

    feat: dynamic

--- frontend/src/app/[lang]/(main)/page.tsx
+++ frontend/src/app/[lang]/(main)/page.tsx
@@ -4,13 +4,29 @@ import BestFeatures from '@/components/LandingPage/BestFeatures';
 import FeaturedBlog from '@/components/LandingPage/FeaturedBlog';
 import SearchBox from '@/components/LandingPage/SearchBox';
 
+// const BestFeatures = lazy(() =>
+//   delayForDemo(import('@/components/LandingPage/BestFeatures'))
+// );
+
+// function delayForDemo(promise: any) {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, 2000);
+//   }).then(() => promise);
+// }
+
 export default function HomePage() {
+  // const [showBestFeatures, setShowBestFeatures] = useState(false);
+
   return (
     <main>
       <section className='bg-white'>
         <SearchBox />
 
+        {/* {showBestFeatures && ( */}
+        {/* <Suspense fallback={<p>Loading...</p>}> */}
         <BestFeatures />
+        {/* </Suspense> */}
+        {/* )} */}
 
         <FeaturedBlog />
       </section>
