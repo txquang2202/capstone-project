89ec470a279a5f0a3258476ea8a8f44381c10b02
txquang2022 <duyhq47@gmail.com>
2024-03-10 17:33:52 +0700

    feat: intergrate jobs-apply-management

--- frontend/src/app/[lang]/(main)/applied-management/page.tsx
+++ frontend/src/app/[lang]/(main)/applied-management/page.tsx
@@ -1,18 +1,18 @@
-'use client';
+// 'use client';
 
-import CompanyCard from '@/components/CompanyPage/CompanyCard';
-import AppliedManagement from '@/components/JobAppliedManagementPage/AppliedManagement';
+// import CompanyCard from '@/components/CompanyPage/CompanyCard';
+// import AppliedManagement from '@/components/JobAppliedManagementPage/AppliedManagement';
 
-export default function JobManageMentPage() {
-  return (
-    <main>
-      <section className=' scroll-smooth bg-[#f2f2f2]  focus:scroll-auto '>
-        {/* Info Company */}
-        <CompanyCard />
-        {/* <Navigation selected='' /> */}
-        <AppliedManagement />
-        {/*  */}
-      </section>
-    </main>
-  );
-}
+// export default function JobManageMentPage() {
+//   return (
+//     <main>
+//       <section className=' scroll-smooth bg-[#f2f2f2]  focus:scroll-auto '>
+//         {/* Info Company */}
+//         <CompanyCard />
+//         {/* <Navigation selected='' /> */}
+//         <AppliedManagement />
+//         {/*  */}
+//       </section>
+//     </main>
+//   );
+// }
